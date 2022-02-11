3b1564ba98d175d62a11e0292316856c1ad90891
Dan Kulov <dankulov78@gmail.com>
2022-02-11 11:42:12 +0200

    learn variables

---
+++ Var/Var.js
@@ -0,0 +1,32 @@
+// let variable
+let name = "Dan";
+console.log(name);
+
+// const variable
+const interestRate = 0.3;
+// Will not work
+interestRate = 1;
+console.log(interestRate);
+
+// Primitive Types
+let name = "Mosh"; // String Literal
+let age = 30; // Number Literal
+let isApproved = false; // Boolean Literal
+let firstName = undefined; // Undefined
+let selectedColor = null; // Object
+
+// Objects
+let person = {
+  name: "Dan",
+  age: 30,
+};
+
+console.log(person);
+
+// Dot Notation
+
+console.log(person.name);
+
+// Bracket Notation
+
+console.log(person["name"]);
