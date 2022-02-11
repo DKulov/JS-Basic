dd0734144c458ea8e228efcf1afa8a23002f5004
Dan Kulov <dankulov78@gmail.com>
2022-02-11 11:42:32 +0200

    learn arrays

---
+++ Array/Array.js
@@ -0,0 +1,10 @@
+let selectedColors = ["Red", "Blue"];
+
+// Adding element in specific location
+selectedColors[2] = "Green";
+
+//Showing all elements
+console.log(selectedColors);
+
+//Showing element in specific location
+console.log(selectedColors[0]);
