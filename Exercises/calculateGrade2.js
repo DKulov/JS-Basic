de0ada55ea548639946300f1d72273a3aa32b153
Dan Kulov <dankulov78@gmail.com>
2022-02-22 18:51:20 +0200

    Exercise Calculate Grade with switch..case and .reduce

--- Exercises/calculateGrade2.js
+++ Exercises/calculateGrade2.js
@@ -1,4 +1,4 @@
-// Exercise calculateGrade (same but with switch..case)
+// Exercise calculateGrade (same but with switch..case and .reduce)
 
 // 1 - 59: F
 // 60 - 69: D
@@ -6,40 +6,34 @@
 // 80 - 89: B
 // 90 - 100: A
 
-const marks = [90, 90, 90];
+const marks = [90, 90, 60];
 
-function calculateAvg(marks) {
-  let sum = 0;
-
-  for (i = 0; i < marks.length; i++) sum += marks[i];
-  let avg = sum / marks.length;
-
-  return avg;
+function getAverage(marks) {
+  return (
+    marks.reduce((total, currentValue) => total + currentValue, 0) /
+    marks.length
+  );
 }
 
-function whatIsMyGrade(marks) {
-  let avg = calculateAvg(marks);
+function averageToGrade() {
+  let average = getAverage(marks);
 
-  switch (avg) {
-    case avg <= 59:
-      return "F";
-      break;
+  switch (true) {
+    case average <= 59:
+      return "Your grade is F";
 
-    case avg <= 69 && avg >= 60:
-      return "D";
-      break;
+    case average <= 69:
+      return "Your grade is D";
 
-    case avg <= 79 && avg >= 70:
-      return "C";
-      break;
+    case average <= 79:
+      return "Your grade is C";
 
-    case avg <= 89 && avg >= 80:
-      return "B";
-      break;
+    case average <= 89:
+      return "Your grade is B";
 
     default:
-      return "A";
+      return "Your grade is A";
   }
 }
 
-console.log(whatIsMyGrade(marks));
+console.log(averageToGrade());
